8ebe388191732a19ef57b8c11ae14eb2781bb2f4
user.email <shijie.bou.d538@dirbato.co.jp>
2025-05-23 15:15:34 +0900

    演算子まで

---
+++ Sample/src/sample/Test.java
@@ -0,0 +1,18 @@
+package sample;
+
+public class Test {
+
+    //mainメソッドはプログラムの最初に作動します。
+    public static void main(String[] args) {
+       /*コメントアウト
+        System.out.println("Kiramex");
+        System.out.println("Java");
+        */
+        //Javaではjavacというソフトウェアがコンパイルを実行する
+        System.out.println("学習を始めましょう！");
+        
+        
+
+    }
+
+}
---
+++ Sample/src/sample/Variable.java
@@ -0,0 +1,24 @@
+package sample;
+
+public class Variable {
+
+    public static void main(String[] args) {
+        var number1 = 1;
+        System.out.print(number1);
+        System.out.println();
+        
+        
+        String str1;
+        str1 = "abc";
+        System.out.print(str1);
+        System.out.println();
+       
+        
+        var str2 = "abc";
+        System.out.println(str2);
+        
+                
+
+    }
+
+}
